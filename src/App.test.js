bef7646a6ddfaed7f1c4beea0935672b8c93a527
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2022-02-07 13:49:27 +0530

    adding adapter

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { configure } from "enzyme";
+import adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
-test('renders learn react link', () => {
+configure({ adapter });
+
+// adapter for react 17: https://stackoverflow.com/questions/64658031/which-enzyme-adapter-works-with-react-17
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
